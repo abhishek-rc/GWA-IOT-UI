689e9a08560bd53cacbd8325b27de8219a663ccd
shahmir-rc <shahmir.k@royalcyber.com>
2025-05-27 01:42:18 +0500

    tanstack query added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
+import ReactQueryProvider from '@/providers/reactQueryProvider';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -17,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
         <ThemeProvider>
-          <SidebarProvider>{children}</SidebarProvider>
+          <SidebarProvider><ReactQueryProvider>{children}</ReactQueryProvider></SidebarProvider>
         </ThemeProvider>
       </body>
     </html>
